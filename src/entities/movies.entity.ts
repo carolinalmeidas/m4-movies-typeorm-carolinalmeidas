212345e8fd68001135b019d7935c22da0e51b0b6
carolinalmeidas <carolinalmeida.s@hotmail.com>
2023-04-24 17:54:03 -0300

    feat: create table movies

--- src/entities/movies.entity.ts
+++ src/entities/movies.entity.ts
@@ -1,3 +1,21 @@
-class Movie {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("movies")
+class Movie {
+  @PrimaryGeneratedColumn("increment")
+  id: number
+
+  @Column({ type: "varchar", length: 50, unique: true })
+  name: string
+
+  @Column({ nullable: true, type: "text" })
+  description?: string | null | undefined
+
+  @Column()
+  duration: number
+
+  @Column()
+  price: number
+}
 
 export default Movie;
