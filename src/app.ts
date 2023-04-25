65bd51664ca38865931085c7324f6119c0562166
carolinalmeidas <carolinalmeida.s@hotmail.com>
2023-04-25 15:54:17 -0300

    create: class AppError and handleErros

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import "reflect-metadata"
+import "express-async-errors"
 import express,  { Application } from "express";
+import { handleErros } from "./error";
 
 const app: Application = express()
 
 app.use(express.json())
+
+app.use(handleErros)
 
 export default app
